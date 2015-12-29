0d8f20f5a59c04a231b64be9a02db46a865c3ee6
GauravDS <gaurav.sharma@punchh.com>
2015-12-29 19:33:47 +0530

    travis CI error

--- ParallaxEffect/AppDelegate.m
+++ ParallaxEffect/AppDelegate.m
@@ -20,7 +20,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
-    return YES;
+//    return YES;
 //    this is non comment code
 }
 							
